bb7a35061690e19f3388fc3bff4c874f2679eded
haspdecrypted <shubham_sen@srmuniv.edu.in>
2023-01-21 22:34:37 +0530

    added square cube file

---
+++ Beginner/Questions/cube_Square_num.java
@@ -0,0 +1,11 @@
+package Beginner.Questions;
+
+public class cube_Square_num {
+    public static void main(String[] args) {
+        int num=4, cube=0,square=0;
+        cube = num * num * num ;
+        square = num * num;
+        System.out.println("square is : " +square);
+        System.out.println("cube is : " +cube);
+    }
+}
